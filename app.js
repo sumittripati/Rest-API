e5d01cecfcb81d073524c9b6296593017e7fb482
tripatisumit <tripathisumit2912@gmail.com>
2025-02-02 01:40:59 +0530

    Updated app.js and db/connect.js

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ let my_routes = require('./Router/myroutes');
 let PORT = process.env.PORT || 3001;
 let connectDB = require('./db/connect');
 
-app.use("/api/products", my_routes);
+app.use("/api", my_routes);
 
 let start = async () => {
     try {
-        await connectDB(process.env.MONGODB_URL);
+        await connectDB(process.env.MONGODB_URI);
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
@@ -18,3 +18,37 @@ let start = async () => {
     }
 }
 start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// require("dotenv").config();
+// let express = require('express');
+// const app = express();
+// let my_routes = require('./Router/myroutes');
+// let PORT = process.env.PORT || 3001;
+// let connectDB = require('./db/connect');
+
+// app.use("/api/products", my_routes);
+
+// let start = async () => {
+//     try {
+//         await connectDB(process.env.MONGODB_URL);
+//         app.listen(PORT, () => {
+//             console.log(`Server is running on port ${PORT}`);
+//         });
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// start();
